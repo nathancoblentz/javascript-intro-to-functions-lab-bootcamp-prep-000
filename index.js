61a066a973390f82395078beec2e7e237234b1b4
nathancoblentz <nathancoblentz@gmail.com>
2017-10-09 20:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
     return loveYouToo
   } else if  (string === string.toUpperCase) {
    return yesIndeed
- } else {
+ } else if (string === string.toLowerCase) {
    return cantHear
 }
 
