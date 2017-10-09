a7f315bbafe59cda5d5ad57b9197eec3dc4f055a
nathancoblentz <nathancoblentz@gmail.com>
2017-10-09 19:20:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout (string) {
+  return string.toUpperCase()
+}
+
+function whisper (string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var loveYouToo= "I love you, too."
+
+    if (string = "I love you, Grandma.") {
+    return loveYouToo
+  }
+}
+
+function sayHiToGrandma(string) {
+    var yesIndded = "YES INDEED!"
+    if (string = string.toUpperCase) {
+     return yesIndeed
+   }
+}
+
+function sayHiToGrandma(string) {
+  var cantHear = "I can't hear you!"
+  if (string = string.toLowerCase) {
+ return cantHear
+}
